c0f67a0ad6d290ec4a8e7ed733f74e9357c41a7d
Mansi Varshney <mansivarshney13@gmail.com>
2023-05-08 16:01:41 +0530

    Initial commit

---
+++ the_weather/weather/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+# Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=85a1dc8bd5141945c45a213138d08da5'
+
+    cities = City.objects.all() #return all the cities in the database
+
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+
+    form = CityForm()
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city.name)).json() #request the API data and convert the JSON to Python data types
+        print(city_weather) #temporarily view output
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list
+
+    context = {'weather_data' : weather_data, 'form' : form}
+
+    return render(request, 'weather/index.html', context) #returns the index.html template
